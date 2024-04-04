bc62903d74a37dacf3aa9f0e474fa03899944ddf
Garegin Epranosyan <gepran@users.noreply.github.com>
2024-04-04 18:14:45 +0400

    Added redirect to mobile app functionaity

--- assets/js/support.js
+++ assets/js/support.js
@@ -16,10 +16,44 @@ document.addEventListener("DOMContentLoaded", () => {
     //const service_fee = "Service Fee";
     const receiver = "Garegin Epranosyan";
     
+
+    const redirectToMobileApp = function() {
+        const userAgent = navigator.userAgent.toLowerCase();
+        const isAndroid = userAgent.indexOf("android") > -1;
+        const isIphone = userAgent.indexOf("iphone") > -1;
+    
+        const url = window.location.href.toLowerCase();
+        var rediredtPath = url.substring(url.indexOf("/mb/") + 4);
+    
+        if (isAndroid) {
+            const url =
+            `intent://tbconline.ge/MB/${rediredtPath}/#Intent;scheme=https;package=com.icomvision.bsc.tbc;end`;
+            window.location.replace(url);
+    
+            setTimeout(() => {
+            window.location.replace(
+                "https://play.google.com/store/apps/details?id=com.icomvision.bsc.tbc"
+            );
+            }, 5000);
+    
+        } else if (isIphone) {
+            window.location.replace(`tbc://${rediredtPath}`);
+    
+            setTimeout(() => {
+            window.location.replace(
+                "https://apps.apple.com/us/app/tbc-bank/id766598432"
+            );
+            }, 5000);
+        } else {
+            window.location.replace("https://tbconline.ge/tbcrd");
+        };
+    };
+
     const copyTextToClipboard = (text) => {
         navigator.clipboard.writeText(text)
         .then(() => {
             messageElement.style.display = "block";
+            redirectToMobileApp();
             setTimeout(() => {
                 messageElement.style.display = "none";
             }, 3000);
@@ -38,6 +72,8 @@ document.addEventListener("DOMContentLoaded", () => {
         ALERT_INNER_TEXT.innerText = "iBan Copied!";
         copyTextToClipboard(bog_account);
     });
+    
+    // redirectToMobileApp();
 
     // SERVICE_FEE.addEventListener("click", () => {
     //     ALERT_INNER_TEXT.innerText = "Text Copied!";
