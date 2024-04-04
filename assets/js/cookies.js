10712af31884123b27b7569d523c08c80667791c
Garegin Epranosyan <gepran@users.noreply.github.com>
2024-04-04 22:37:31 +0400

    asd

---
+++ assets/js/cookies.js
@@ -0,0 +1,45 @@
+function generateUniqueId() {
+    // Get current timestamp
+    var timestamp = new Date().getTime();
+
+    // Generate a random number
+    var randomNumber = Math.random() * Math.pow(10, 17); // Adjust the power of 10 as needed for uniqueness
+
+    // Concatenate timestamp and random number
+    var uniqueId = timestamp.toString() + randomNumber.toString();
+
+    return uniqueId;
+}
+
+
+// Function to set a cookie with a unique identifier
+function setDeviceCookie() {
+    var deviceId = generateUniqueId(); // You would need to implement generateUniqueId() function
+    document.cookie = "device_id=" + deviceId + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+}
+
+// Function to get the value of the device cookie
+function getDeviceCookie() {
+    var name = "device_id=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) == 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+// Example usage
+var deviceId = getDeviceCookie();
+if (!deviceId) {
+    setDeviceCookie();
+    console.log("Device cookie set.");
+} else {
+    console.log("Device cookie found: " + deviceId);
+}
