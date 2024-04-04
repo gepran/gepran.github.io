bfef44fc63cfeb66ac1a657156a96ba848bfe6bc
Garegin Epranosyan <gepran@users.noreply.github.com>
2024-04-04 20:29:55 +0400

    jahjsgd

--- assets/js/support.js
+++ assets/js/support.js
@@ -10,6 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
     //const SERVICE_FEE = document.getElementById("service-fee");
     const RECEIVER = document.getElementById("boombuli-receiver");
 
+    const tbc_scheme_url = "https://tbconline.ge/MB/TT"; //"tbc://";
+    const bog_scheme_url = "bogmBank://";
     
     const tbc_account = "GE69TB7003612365100038";
     const bog_account = "GE25BG0000000542914598";
@@ -17,7 +19,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const receiver = "Garegin Epranosyan";
     
 
-    const redirectToMobileApp = function() {
+    const androidRedirect = function(scheme_url, iBan, rediredtPath) {
+        const url = `intent://tbconline.ge/MB/${rediredtPath}/#Intent;scheme=https;package=com.icomvision.bsc.tbc;end`;
+        window.location.replace(url);
+
+        setTimeout(() => {
+        window.location.replace(
+            "https://play.google.com/store/apps/details?id=com.icomvision.bsc.tbc"
+        );
+        }, 5000);
+    }
+
+    const iphoneRedirect = function(scheme_url, iBan) {
+        // https://tbconline.ge/MB/TT
+        window.location.replace(`${scheme_url}/${iBan}`);
+    
+        setTimeout(() => {
+        window.location.replace(
+            "https://apps.apple.com/us/app/tbc-bank/id766598432"
+        );
+        }, 5000);
+    }
+
+    const redirectToMobileApp = function(scheme_url, iBan) {
         const userAgent = navigator.userAgent.toLowerCase();
         const isAndroid = userAgent.indexOf("android") > -1;
         const isIphone = userAgent.indexOf("iphone") > -1;
@@ -26,34 +50,19 @@ document.addEventListener("DOMContentLoaded", () => {
         var rediredtPath = url.substring(url.indexOf("/mb/") + 4);
     
         if (isAndroid) {
-            const url =
-            `intent://tbconline.ge/MB/${rediredtPath}/#Intent;scheme=https;package=com.icomvision.bsc.tbc;end`;
-            window.location.replace(url);
-    
-            setTimeout(() => {
-            window.location.replace(
-                "https://play.google.com/store/apps/details?id=com.icomvision.bsc.tbc"
-            );
-            }, 5000);
-    
+            androidRedirect(scheme_url, iBan, rediredtPath);
         } else if (isIphone) {
-            window.location.replace(`https://tbconline.ge/MB/TT/${tbc_account}`);
-    
-            setTimeout(() => {
-            window.location.replace(
-                "https://apps.apple.com/us/app/tbc-bank/id766598432"
-            );
-            }, 5000);
+            iphoneRedirect(scheme_url, iBan);
         } else {
             window.location.replace("https://tbconline.ge/tbcrd");
         };
     };
 
-    const copyTextToClipboard = (text) => {
-        navigator.clipboard.writeText(text)
+    const copyTextToClipboard = (scheme_url, iBan) => {
+        navigator.clipboard.writeText(iBan)
         .then(() => {
             messageElement.style.display = "block";
-            redirectToMobileApp();
+            redirectToMobileApp(scheme_url, iBan);
             setTimeout(() => {
                 messageElement.style.display = "none";
             }, 3000);
@@ -65,12 +74,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     TBC_BUTTON.addEventListener("click", () => {
         ALERT_INNER_TEXT.innerText = "iBan Copied!";
-        copyTextToClipboard(tbc_account);
+        copyTextToClipboard(tbc_scheme_url, tbc_account);
     });
 
     BOG_BUTTON.addEventListener("click", () => {
         ALERT_INNER_TEXT.innerText = "iBan Copied!";
-        copyTextToClipboard(bog_account);
+        copyTextToClipboard(bog_scheme_url, bog_account);
     });
     
     // redirectToMobileApp();
